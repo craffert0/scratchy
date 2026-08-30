1e0153c1b8b8a9ce575a3f9dd33d84d3a5136ac0
Colin Rafferty <colin@rafferty.net>
2026-08-30 16:53:43 -0400

    update to swiftutil/1.2.0 with its InfoView

--- app/Scratchy/InfoView.swift
+++ app/Scratchy/InfoView.swift
@@ -3,11 +3,9 @@
 
 import SwiftUI
 import SwiftUIUtil
+import SwiftUIUtilModel
 
 struct InfoView: View {
-    private let githubMarkdown =
-        "[git@github.com:craffert0/scratchy]" +
-        "(https://github.com/craffert0/scratchy)"
     private let instructions = [
         """
         Type any text on this device, and it will be updated on all your
@@ -19,48 +17,14 @@ struct InfoView: View {
         """,
     ]
 
-    @State private var showLicense: Bool = false
-
     var body: some View {
-        Form {
-            instructionsView
-            copyrightView
-        }
-        .navigationTitle("Scritchy")
-        .navigationBarTitleDisplayMode(.large)
-    }
-
-    private var instructionsView: some View {
-        Section("") {
-            ForEach(instructions, id: \.self) {
-                Text($0.unwrapped)
-                    .textSelection(.enabled)
-            }
-        }
-    }
-
-    private var copyrightView: some View {
-        Section {
-            Text(
-                "Copyright © 2026 Colin Rafferty"
-            )
-            .multilineTextAlignment(.center)
-            .frame(maxWidth: .infinity, alignment: .center)
-
-            Button("Licensed GNU GPLv2.0") {
-                showLicense = true
-            }
-            .multilineTextAlignment(.center)
-            .frame(maxWidth: .infinity, alignment: .center)
-            .sheet(isPresented: $showLicense) {
-                LicenseView(model: .gplV2)
-            }
-
-            Text(
-                try! AttributedString(markdown: githubMarkdown)
-            )
-            .frame(maxWidth: .infinity, alignment: .center)
-        }
+        SwiftUIUtil.InfoView(
+            title: "Scritchy",
+            repo: .github(user: "craffert0", repo: "scratchy"),
+            copyright: "2026 Colin Rafferty",
+            license: .gplV2,
+            instructions: instructions
+        )
     }
 }
 
